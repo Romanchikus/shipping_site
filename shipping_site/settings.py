1054db56ec4080dc8d6c13c3727088f7f92c099c
Romanchikus <13ternopil@gmail.com>
2020-10-11 16:16:47 +0300

    set static

--- shipping_site/settings.py
+++ shipping_site/settings.py
@@ -139,12 +139,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "'staticfiles')")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static_in_dev"),)
+
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 # CHANNEL_LAYERS = {
